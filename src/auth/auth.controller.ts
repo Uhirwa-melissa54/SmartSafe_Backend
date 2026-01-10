4512fa12f66ac013592297ff6b0fb1907da527f8
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-10 22:12:45 +0200

    did the first controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Post,Body, Controller } from "@nestjs/common";
+import { UserDto } from "src/Dto/UserDto";
 
 @Controller("auth")
 export class AuthController{
     @Post('signup')
-    async signUp(){
-        
+    async signUp(@Body() user:UserDto){
+        const user=
+
     }
 }
