0075ae318575f71afd695c1a7d99e638a8d12478
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-11 15:25:50 +0200

    created new module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import * as argon2 from "argon2"
+
+import { UserDto } from "src/Dto/UserDto";
 
 @Injectable()
 export class AuthService{
-    
+ AuthService(){
+
+}
+    async signUp(user:UserDto) {
+        const hashedPassword=argon2.hash(user.password,{
+            type:argon2.argon2id
+        }
+            
+        )
+        
+    }
+    const createdUser=
 }
