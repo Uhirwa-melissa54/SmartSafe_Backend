c659149c2120df4a33eced5d95063ad02956adad
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-11 15:41:53 +0200

    finalized signUp method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
 
 @Module({})
-export class AuthModule {}
+export class AuthModule {
+    imports:[PrismaModule]
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from "@nestjs/common";
 import * as argon2 from "argon2"
 
 import { UserDto } from "src/Dto/UserDto";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService{
- AuthService(){
+ constructor(private prisma:PrismaService){
 
 }
     async signUp(user:UserDto) {
@@ -14,7 +15,18 @@ export class AuthService{
         }
             
         )
+          const createdUser=this.prisma.user.create({
+            data:{
+                firstName:user.firstName,
+                lastName:user.lastName,
+                password:hashedPassword,
+                phoneNumber:user.phoneNumber
+
+            }
+        
+    })
+    return createdUser
         
     }
-    const createdUser=
+  
 }
